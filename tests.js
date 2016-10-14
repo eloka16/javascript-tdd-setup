8e9365a0ed674e7510a52e440ba09af7ee788756
frank chukwu <frank chukwu>
2016-10-15 00:32:31 +0100

    Create test case for positive numbers less than or equal to 2

--- tests.js
+++ tests.js
@@ -15,4 +15,13 @@ describe("Test sum of prime numbers less than or equal to a number", function() 
     assert(
       lib.sumOfPrimes(10) == 17);
   });
+  it("should return 2 if number is 2", function() {
+  assert(
+    lib.sumOfPrimes(2) == 2);
+  });
+  it("should return undefined if number is less than 2", function () {
+    assert(
+      lib.sumOfPrimes(1) == undefined
+    )
+  })
 });
